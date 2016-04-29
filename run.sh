638edcab7a404be80db0b61329422196be99b63c
markito3 <marki@jlab.org>
2016-04-29 10:22:41 -0400

    use standard jlab gluex set-up

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source /group/halld/Repositories/build_scripts/gluex_env_jlab.sh
+source /group/halld/Software/build_scripts/gluex_env_jlab.sh
 hdgeant
 mcsmear hdgeant.hddm
 hd_root -PPLUGINS=monitoring_hists,danarest,pi0_pol_plugin hdgeant_smeared.hddm
