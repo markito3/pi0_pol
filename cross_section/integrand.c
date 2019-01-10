32308139155890bc30559f9fbdb3f943cc204c79
markito3 <marki@jlab.org>
2019-01-10 16:58:30 -0500

    computes real and imaginary parts of integral

--- cross_section/integrand.c
+++ cross_section/integrand.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_integration.h>
+#define PI 3.14159265359
 
 struct integrand_params {
-  double a;
-  double b;
+  double s;
+  double mpi;
+  int r_or_i;
 };
 
 double funcx(double x, void *parameters) {
   double c, y;
   struct integrand_params *these_params = (struct integrand_params *)parameters; 
-  c = (these_params->a);
+  c = (these_params->s);
   y = c*x*x;
   printf("c = %f, x = %f, y = %f\n", c, x, y);
   return y;
@@ -18,11 +20,22 @@ double funcx(double x, void *parameters) {
 
 double func(double x, void *parameters) {
   double s, mpi, arg, y;
+  int r_or_i;
   struct integrand_params *these_params = (struct integrand_params *)parameters; 
-  s = (these_params->a);
-  mpi = (these_params->b);
-  arg = 1.0 - (s/(mpi*mpi))*x*(1.0 - x);
-  y = log(arg)/x;
+  s = (these_params->s);
+  mpi = (these_params->mpi);
+  r_or_i = these_params->r_or_i;
+  arg = 1.0d - (s/(mpi*mpi))*x*(1.0d - x);
+  if (r_or_i == 0) { /* real part */
+    y = log(fabs(arg))/x;
+  } else { /* imaginary part */
+    if (arg >= 0.0d) {
+      y = 0.0d;
+    } else {
+      y = -PI/x;
+    }
+  }
+
   printf("s = %f, mpi = %f, x = %f, arg = %f, y = %f\n", s, mpi, x, arg, y);
   return y;
 }
@@ -51,14 +64,15 @@ int main() {
   int limit;
 
   mpi = 135.0;
-  s = 4*mpi*mpi - 10.0;
+  s = 4*mpi*mpi + .001 + 5000.;
 
   integrand.function = &func;
-  params_in.a = s;
-  params_in.b = mpi;
+  params_in.s = s;
+  params_in.mpi = mpi;
+  params_in.r_or_i = 1;
   integrand.params = &params_in;
 
-  n = 1024;
+  n = 1024*128;
   limit = (int)n;
   workspace = gsl_integration_workspace_alloc(n);
 
