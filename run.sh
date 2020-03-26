148e659c1844a6798ad7c90ed89e9c5fb68459a4
markito3 <marki@jlab.org>
2020-03-26 14:38:30 -0400

    convert to use hdgeant4

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 source /group/halld/Software/build_scripts/gluex_env_jlab.sh
 export JANA_CALIB_CONTEXT="variation=mc_sim1"
-hdgeant
+echo "Running hdgeant4 ..."
+rm -fv run.mac
+cat > run.mac <<EOF
+/run/beamOn 10000
+EOF
+command="hdgeant4 run.mac"
+echo command = $command
+$command
 mcsmear hdgeant.hddm
 hd_root -PJANA:BATCH_MODE=1 -PPLUGINS=monitoring_hists,danarest,pi0_pol_plugin hdgeant_smeared.hddm
