b8525ad8e6a5723d6b738c62a86d560bbff63b6d
Mark Ito <markito3@gmail.com>
2021-05-22 12:44:56 -0400

    Remove run scripts. Use the makefile instead.

--- run_home.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-setup_gluex
-export JANA_CALIB_CONTEXT="variation=mc_sim1"
-hdgeant
-mcsmear hdgeant.hddm
-hd_root -PJANA:BATCH_MODE=1 -PPLUGINS=monitoring_hists,danarest,pi0_pol_plugin hdgeant_smeared.hddm
